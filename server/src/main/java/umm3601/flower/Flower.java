14d3b36ecae3aef5d05fa86cb985db8771cb3648
tszhonglau <lauxx265@morris.umn.edu>
2017-03-26 16:57:50 -0500

    tried to implement xml file importing into the database but failed.
    nullpointerexception

--- server/src/main/java/umm3601/flower/Flower.java
+++ server/src/main/java/umm3601/flower/Flower.java
@@ -1,10 +1,15 @@
 package umm3601.flower;
 
+import org.bson.types.ObjectId;
+
 public class Flower {
-    String _id;
-    String commonName;
-    String cultivar;
-    String source;
-    String gardenLocation;
-    int year;
+    public ObjectId _id;
+    public String commonName;
+    public String cultivar;
+    public String HBaHangaBasketCaContainerWaWall;
+    public String gardenLocation;
+    public String Comments;
+    public String id;
+    public String source;
+    public String SaSeedVaVeg;
 }
