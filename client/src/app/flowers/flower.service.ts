ecea0c17b819938189b85182e68cb11cec6ef4ed
Jeiloss <15thirty>
2017-03-30 01:49:05 -0500

    Add file upload to the clientside
    #1

--- client/src/app/flowers/flower.service.ts
+++ client/src/app/flowers/flower.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, RequestOptions, Headers} from '@angular/http';
 import { Observable } from "rxjs";
 import { Flower } from "./flower";
 
@@ -7,6 +7,7 @@ import { Flower } from "./flower";
 export class FlowerService {
     private bedUrl: string = API_URL + "beds";
     private flowerUrl: string = API_URL + "flowers";
+    private uploadUrl: string = API_URL + "flowers/upload";
     constructor(private http:Http) { }
 
     getFlower(garden: string, cultivar:string): Observable<any> {
@@ -21,5 +22,30 @@ export class FlowerService {
         return this.http.request(this.bedUrl).map(res => res.json());
     }
 
+    uploadFile(event) {
+        let files = event.target.files;
+        if (files.length > 0) {
+            let formData: FormData = new FormData();
+            for (let file of files) {
+                formData.append('files', file, file.name);
+            }
+            console.log(formData);
+            let headers = new Headers();
+            headers.set('Accept', 'application/json');
+            let options = new RequestOptions({ headers: headers });
+            this.http.post(this.uploadUrl, formData, options)
+                .map(res => res.json())
+                .catch(error => Observable.throw(error))
+                .subscribe(
+                    data => {
+                        // Consume Files
+                        // ..
+                        console.log('uploaded and processed files');
+                    },
+                    error => console.log(error),
+                );
+            console.log("file sent");
+        }
+    }
 
 }
